60ae26b95c8f2260d75585e8e7d3e13f3f75fd53
owen-cox <owen-cox@uiowa.edu>
2026-04-09 11:38:49 -0500

    Converting circle objects to images

--- Registration/Registration.cxx
+++ Registration/Registration.cxx
@@ -44,8 +44,6 @@ drawCircle(double radius, double x, double y) {
     transform->Translate(translation, false);
     circle->SetObjectToParentTransform(transform);
 
-    // I probably want to make this an image
-
     return circle;
 }
 
@@ -54,19 +52,46 @@ int main(int argc, char* * argv) {
 
     constexpr size_t Dimension = 2;
 
-    using EllipseType = itk::EllipseSpatialObject<Dimension>;
+    using CircleType = itk::EllipseSpatialObject<Dimension>;
     using PixelType = float;
+    using ImageType = itk::Image<PixelType, Dimension>;
 
     //auto circle = EllipseType::New();
 
     auto circle0 = drawCircle<Dimension>(15.0, 50.0, 50.0);
     auto circle1 = drawCircle<Dimension>(30.0, 200.0, 200.0);
 
-    // Probably want to use a similarity transform since we are only doing translation and scaling
-
-    // How do I turn circles into an image?
     using FixedImageType = itk::Image<PixelType, Dimension>;
     using MovingImageType = itk::Image<PixelType, Dimension>;
+
+    using RasterizeFilterType = itk::SpatialObjectToImageFilter<CircleType, ImageType>;
+
+    auto rasterize = RasterizeFilterType::New();
+    rasterize->SetInput(circle0);
+
+    ImageType::SizeType size;
+    size[0] = 512;
+    size[1] = 512;
+    rasterize->SetSize(size);
+
+    ImageType::SpacingType spacing;
+    spacing[0] = 1.0;
+    spacing[1] = 1.0;
+    rasterize->SetSpacing(spacing);
+
+    ImageType::PointType origin;
+    origin[0] = 0.0;
+    origin[1] = 0.0;
+    rasterize->SetOrigin(origin);
+
+    rasterize->SetInsideValue(255);
+    rasterize->SetOutsideValue(0);
+
+    rasterize->Update();
+
+
+
+    // Probably want to use a similarity transform since we are only doing translation and scaling
 
     using TransformType = itk::TranslationTransform<double, Dimension>;
     using OptimizerType = itk::RegularStepGradientDescentOptimizerv4<double>;
