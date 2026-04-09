dd276a6ae81f2300cf64a7efd8646e219cbbaa7e
owen-cox <owen-cox@uiowa.edu>
2026-04-09 10:14:10 -0500

    Minor build config fixes and ellipse setup

--- Registration/Registration.cxx
+++ Registration/Registration.cxx
@@ -2,15 +2,27 @@
 // Created by ocx on 4/9/26.
 //
 
-#include "EdgeSlicerModuleCLP.h"
+#include "RegistrationCLP.h"
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include <itkSobelEdgeDetectionImageFilter.h>
+
+
+// Ellipse function sounds useful
+#include "itkEllipseSpatialObject.h"
+
 
 int main(int argc, char* * argv) {
     PARSE_ARGS;
 
-    constexpr size_t Dimension = 3;
+    constexpr size_t Dimension = 2;
+
+    using EllipseType = itk::EllipseSpatialObject<Dimension>;
+    using PixelType = float;
+
+    auto circle = EllipseType::New();
+
+
+    return EXIT_SUCCESS;
 
 }
