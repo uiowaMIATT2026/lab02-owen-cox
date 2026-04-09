4ae69a459d02bf51ee63a3b690bb3e843ceda395
owen-cox <owen-cox@uiowa.edu>
2026-04-09 11:04:35 -0500

    drawCircle draft

--- Registration/Registration.cxx
+++ Registration/Registration.cxx
@@ -15,7 +15,7 @@
 // Design for Dimension == 2 for now
 template <unsigned int Dimension>
 typename itk::EllipseSpatialObject<Dimension>
-drawDircles() {
+drawCircle() {
     using EllipseType = itk::EllipseSpatialObject<Dimension>;
 
     auto circle = EllipseType::New();
@@ -23,6 +23,18 @@ drawDircles() {
     // Set radius, position
     // First circle radius 30.0 mm -> diameter 60.0
     circle->SetRadiusInObjectSpace(60.0);
+
+    // We need perform a translation to set circle position
+    using TransformType = typename EllipseType::TransformType;
+    auto transform = TransformType::New();
+    transform->SetIdentity();
+
+    typename TransformType::OutputVectorType translation;
+    translation[0] = 50.0;
+    translation[1] = 50.0;
+
+    transform->Translate(translation, false);
+    circle->SetObjectToParentTransform(transform);
 
     return circle;
 }
@@ -35,7 +47,9 @@ int main(int argc, char* * argv) {
     using EllipseType = itk::EllipseSpatialObject<Dimension>;
     using PixelType = float;
 
-    auto circle = EllipseType::New();
+    //auto circle = EllipseType::New();
+
+    auto circle = drawCircle<2>();
 
 
     return EXIT_SUCCESS;
